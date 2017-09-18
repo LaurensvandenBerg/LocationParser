3b7eb39710df0069ae16f04b98ebe07697696f3b
LaurensOomen <oomen.laurens@gmail.com>
2017-09-18 09:57:54 +0200

    Command factory tests and gitignore update

---
+++ LocationParserTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LocationParserTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LocationParserTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3311da0e-f1cd-457d-8e2e-6770f4419083")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LocationParserTests/UnitTests/CommandFactoryTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LocationParser.Commands;
+using System.Linq;
+
+namespace LocationParserTests.UnitTests
+{
+	[TestClass]
+	public class CommandFactoryTests
+	{
+		[TestMethod]
+		public void CommandBinding_WhenRegisteringACommand_ItShouldBeDiscoverableByItsParent()
+		{
+			//Arrange
+			var parentCommand = new CommandLineApplication();
+			var factory = new CommandFactory(parentCommand);
+
+			//Act
+			factory.Register<StubCommand>();
+
+			//Assert
+			Assert.AreEqual(1,parentCommand.Commands.Count);
+			Assert.IsInstanceOfType(typeof(StubCommand),parentCommand.Commands.FirstOrDefault().GetType());
+		}
+		private class StubCommand : Command
+		{
+			public StubCommand(CommandLineApplication parent) : base(parent)
+			{
+			}
+
+			public override void SetupCommand()
+			{
+				var stub = CreateCommand("stubCommand", "A stub Command used for testing");
+			}
+		}
+	}
+}
