01656cb9e87c80c914b609b64bc63fb46f7f412f
laurens-van-den-berg <laurensvdberg94@gmail.com>
2018-03-12 16:57:25 +0100

    zipping cleanup

--- src/Commands/FileIO/ReadCommand.cs
+++ src/Commands/FileIO/ReadCommand.cs
@@ -3,10 +3,9 @@ using LocationParser.Extensions.Models;
 using LocationParser.Models.Google;
 using ConsoleAppBase.Attributes;
 using Newtonsoft.Json;
-using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
+using LocationParser.Data.External;
 
 namespace LocationParser.Commands.FileIO
 {
@@ -16,10 +15,10 @@ namespace LocationParser.Commands.FileIO
 		[CommandArgument(1, Name = "path", Description = "The path of the JSON file with the locations.", Required = true)]
 		public string TimeLinePath { get; set; }
 
-		[CommandOption(Template = "-m|--multiple", Description = "flag to read all files in a given directory")]
+		[CommandOption(Template = "-m|--multiple", Description = "Flag to read all files in a given directory.")]
 		public bool MultipleFiles { get; set; }
 
-		[CommandOption(Template = "-z|--zipped")]
+		[CommandOption(Template = "-z|--zipped", Description = "Flag to read zipped timelines.")]
 		public bool Zipped { get; set; }
 
 		public override int OnExecute()
@@ -28,18 +27,10 @@ namespace LocationParser.Commands.FileIO
 			{
 				ShowHelp();
 			}
-			var timeLinePaths = GetTimeLines();
 
-			var mergedTimeLine = new Models.Internal.TimeLine()
-			{
-				timeEntries = timeLinePaths
-					.SelectMany(p => JsonConvert.DeserializeObject<Locations>(File.ReadAllText(p))
-						.ToTimeLine()
-						.timeEntries)
-			};
+			var mergedTimeLine = FileInteraction.GetTimeLinesFrom(TimeLinePath, MultipleFiles, Zipped);
+
 			CurrentFile.Write(mergedTimeLine);
-
-			CleanupZipFiles();
 			return 0;
 		}
 
@@ -51,59 +42,5 @@ namespace LocationParser.Commands.FileIO
 			}
 			return File.Exists(TimeLinePath);
 		}
-
-		private IEnumerable<string> GetTimeLines()
-		{
-			var paths = new[] { TimeLinePath };
-			if (Zipped)
-			{
-				UnzipAllTimeLines();
-			}
-			if (MultipleFiles)
-			{
-				paths = Directory.GetFiles(TimeLinePath, "*.json");
-			}
-			return paths;
-		}
-
-		private void UnzipAllTimeLines()
-		{
-			var zipFiles = Directory.GetFiles(TimeLinePath, "*.zip").ToList();
-			var extractionPaths = Enumerable.Range(0, zipFiles.Count).Select(i => Path.Combine(TimeLinePath, "ExtractedTimeLines") + i).ToList();
-			
-			for(int i = 0; i < zipFiles.Count(); i++)
-			{
-				ZipFile.ExtractToDirectory(zipFiles.ElementAt(i), extractionPaths.ElementAt(i));
-			}
-
-			for(int i = 0; i < zipFiles.Count(); i++)
-			{
-				var sourcePath = Path.Combine(extractionPaths.ElementAt(i), GetLanguage(extractionPaths.ElementAt(i)));
-				var destinationPath = Path.Combine(TimeLinePath, "Hist" + i + ".json");
-				File.Move(sourcePath, destinationPath);
-			}
-			
-			extractionPaths.ForEach(p => Directory.Delete(p, true));
-		}
-
-		private void CleanupZipFiles()
-		{
-			var paths = Directory.GetFiles(TimeLinePath, "Hist*.json").ToList();
-			paths.ForEach(p => File.Delete(p));
-		}
-
-		private string GetLanguage(string path)
-		{
-			if (File.Exists(Path.Combine(path, "Takeout/Location History/Location History.json")))
-			{
-				return "Takeout/Location History/Location History.json";
-			}
-			if(File.Exists(Path.Combine(path, "Takeout/Locatiegeschiedenis/Locatiegeschiedenis.json")))
-			{
-				return "Takeout/Locatiegeschiedenis/Locatiegeschiedenis.json";
-			}
-			return "pathnotfound";
-		}
-		
 	}
 }
---
+++ src/Data/External/FileInteraction.cs
@@ -0,0 +1,82 @@
+﻿using LocationParser.Extensions.Models;
+using LocationParser.Models.Google;
+using LocationParser.Models.Internal;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace LocationParser.Data.External
+{
+	class FileInteraction
+	{
+		internal static TimeLine GetTimeLinesFrom(string path, bool multiple, bool zipped)
+		{
+			var timeLinePaths = GetTimeLines(path, multiple, zipped);
+
+			var mergedTimeLine = new TimeLine()
+			{
+				timeEntries = timeLinePaths
+					.SelectMany(p => JsonConvert.DeserializeObject<Locations>(File.ReadAllText(p))
+						.ToTimeLine()
+						.timeEntries)
+			};
+			CleanupZipFiles(path);
+			return mergedTimeLine;
+		}
+
+		internal static IEnumerable<string> GetTimeLines(string path, bool multiple, bool zipped)
+		{
+			var paths = new[] { path };
+			if (zipped)
+			{
+				UnzipAllTimeLines(path);
+			}
+			if (multiple)
+			{
+				paths = Directory.GetFiles(path, "*.json");
+			}
+			return paths;
+		}
+
+		private static void UnzipAllTimeLines(string path)
+		{
+			var zipFiles = Directory.GetFiles(path, "*.zip").ToList();
+			var extractionPaths = Enumerable.Range(0, zipFiles.Count).Select(i => Path.Combine(path, "ExtractedTimeLines") + i).ToList();
+
+			for (int i = 0; i < zipFiles.Count(); i++)
+			{
+				ZipFile.ExtractToDirectory(zipFiles.ElementAt(i), extractionPaths.ElementAt(i));
+			}
+
+			for (int i = 0; i < zipFiles.Count(); i++)
+			{
+				var sourcePath = Path.Combine(extractionPaths.ElementAt(i), GetLanguage(extractionPaths.ElementAt(i)));
+				var destinationPath = Path.Combine(path, "Hist" + i + ".json");
+				File.Move(sourcePath, destinationPath);
+			}
+
+			extractionPaths.ForEach(p => Directory.Delete(p, true));
+		}
+
+		private static string GetLanguage(string path)
+		{
+			if (File.Exists(Path.Combine(path, "Takeout/Location History/Location History.json")))
+			{
+				return "Takeout/Location History/Location History.json";
+			}
+			if (File.Exists(Path.Combine(path, "Takeout/Locatiegeschiedenis/Locatiegeschiedenis.json")))
+			{
+				return "Takeout/Locatiegeschiedenis/Locatiegeschiedenis.json";
+			}
+			return "pathnotfound";
+		}
+
+		private static void CleanupZipFiles(string path)
+		{
+			var paths = Directory.GetFiles(path, "Hist*.json").ToList();
+			paths.ForEach(p => File.Delete(p));
+		}
+	}
+}
